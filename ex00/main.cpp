f989269d02f566401df394975e2d4dc3c7fe0c0b
Andrei Kretov <akretov@student.42prague.com>
2025-02-23 18:45:28 +0100

    added logic and containers. I need to check info on containers and then follow the logic.txt

---
+++ ex00/main.cpp
@@ -0,0 +1,13 @@
+#include "BitcoinExchange.hpp"
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    if (argc == 2) {
+        
+    }
+    else {
+        std::cerr << "Please add file as argument: ./btc example.txt\n";
+        return -1;
+    }
+    return 0;
+}
