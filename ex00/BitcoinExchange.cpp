20d2eeed2024f18907912e9a7043a4943aff065f
Andrei Kretov <akretov@student.42prague.com>
2025-02-25 19:16:42 +0100

    Fixed an issue with Makefile, adding additional flag -g as make Debug wasnt working so i deleted it. ex00: correctly getting information to map, the problem is i need to think how i should handle incorrect value and 0.

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -1,1 +1,3 @@
 #include "BitcoinExchange.hpp"
+
+
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,13 +1,73 @@
 #include "BitcoinExchange.hpp"
 #include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
+#include <stdexcept>
+#include <map>
+#include <string>
+#include <cstdlib>
 
 int main(int argc, char *argv[]) {
-    if (argc == 2) {
-        
-    }
-    else {
+    if (argc != 2) {
         std::cerr << "Please add file as argument: ./btc example.txt\n";
         return -1;
     }
+
+    (void)argv;
+
+    std::ifstream                       iFile;
+    std::string                         line = "";
+    std::map<std::string, double>       data;
+    std::string                         date, valueStr;
+    double                              value = 0;
+      
+    try {
+        iFile.open("data.csv");
+        if (!iFile.is_open()) {
+            throw std::runtime_error("File could not be opened.");
+        }
+    }
+    catch(std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+
+    std::cout << "starting to read from file\n";
+    while (getline(iFile, line)) {
+        if (line.empty()) {
+            continue;
+        }
+        // working with line directly
+        size_t commaPos = line.find(',');
+        if (commaPos != std::string::npos) {
+            std::string date = line.substr(0, commaPos);
+            std::string valueStr = line.substr(commaPos + 1);
+            //check for atof 0 return
+            value = std::atof(valueStr.c_str());
+            if (value == 0 && valueStr != "0") {
+            // Handle invalid value case
+                data[date] = -1;  // Store "NoN" if conversion failed
+            } else {
+                data[date] = value;  // Store valid numeric value
+            }
+        }
+        // // example with working
+        // std::stringstream ss(line);
+        // if (std::getline(ss, date, ',') && std::getline(ss, valueStr)) {
+        //     std::stringstream valueStream(valueStr);
+        //     double value = 0.0;
+        //     valueStream >> value;
+
+        //     data[date] = value;
+        // }
+    }
+
+    iFile.close();
+    //print map
+    for (std::map<std::string, double>::const_iterator it = data.begin(); it != data.end(); ++it) {
+        std::cout << "Date: " << it->first << ", Value: " << it->second << std::endl;
+    }
+
     return 0;
 }
