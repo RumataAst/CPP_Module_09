9c8a483acae928665b9d2c8aa9034cd8d151d9d8
Andrei <krestonosa@gmail.com>
2025-03-04 21:48:15 +0100

    didnt have time for alg, was doing leetcode. Before doing algorithm i need to go through the theory

---
+++ ex02/include/PmergeMe.hpp
@@ -0,0 +1,20 @@
+# ifndef  PMERGE_ME_HPP
+#define PMERGE_ME_HPP
+
+#include <iostream>
+#include <deque>
+#include <vector>
+
+class Alg {
+private:
+    std::vector<int>    ;
+    std::deque
+public:
+    Alg();
+    Alg(const Alg &copy);
+    Alg &operator=(const Alg &source);
+    ~Alg();
+
+}
+
+#endif
