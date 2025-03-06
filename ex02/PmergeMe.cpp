81a5eae77477142a5601d7398393bac9e6600fb6
Andrei <krestonosa@gmail.com>
2025-03-06 22:00:40 +0100

    Adding the second step, sorting based on how many %2 elements are there. For example number of elements is 9. I look the largest power of 2 in 9 which is 3 2^3 = 8. Then i start working with the groups. First 8 contains 4 groups with 2 elements. Next step we increase group size to 4 meaning now we have only 2 groups with 4 elements to compare. Comparason happens for the last element of the group if it's larget than the last element of secodn group then we swap groups

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -54,52 +54,47 @@ void    Alg::print_result() const {
     std::cout << std::endl;
 }
 
-void   Alg::sort_vector_seq() {
-    int     number_swaps = 0;
+void     Alg::sort_vector_seq() {
+    int number_swaps = 0;
+    size_t vector_size = vector_seq.size();
 
-    std::cout << "Fist step\n";
+    std::cout << "Initial Vector:\n";
     print_vector(vector_seq);
 
-    std::vector<int>::iterator end_it;
-    if (vector_seq.size() % 2 == 0) {
-        end_it = vector_seq.end();
-    }
-    else {
-        end_it = vector_seq.end() - 1;
-    }
-    for (std::vector<int>::iterator start_it = vector_seq.begin(); start_it != end_it; start_it+=2) {
-        if (*start_it > *(start_it+1)) {
-            std::swap(*start_it, *(start_it+1));
+    // First step: Swap adjacent elements
+    for (size_t i = 0; i + 1 < vector_size; i += 2) {
+        if (vector_seq[i] > vector_seq[i + 1]) {
+            std::swap(vector_seq[i], vector_seq[i + 1]);
             number_swaps++;
         }
     }
 
-    std::cout << "Second step\n";
+    std::cout << "After first step:\n";
     print_vector(vector_seq);
 
-    for (std::vector<int>::iterator start_it = vector_seq.begin(); start_it != end_it; start_it+=4) {
-        if (*(start_it+1) > *(start_it+2)) {
-            std::swap(*start_it, *(start_it+2));
-            std::swap(*(start_it+1), *(start_it+3));
-            number_swaps++;
+    size_t max_power_of_2 = 1;
+    while (max_power_of_2 * 2 <= vector_size) {
+        max_power_of_2 *= 2;
+    }
+
+    size_t last1 = 0;
+    size_t last2 = 0;
+    for (size_t group_size = max_power_of_2; group_size >= 2; group_size /= 2) {
+        for (size_t i = 0; i + group_size * 2 - 1 < vector_size; i += group_size * 2) {
+            last1 = i + group_size - 1;
+            last2 = i + 2 * group_size - 1;
+
+            if (vector_seq[last1] > vector_seq[last2]) {
+                for (size_t j = 0; j < group_size; ++j) {
+                    std::swap(vector_seq[i + j], vector_seq[i + j + group_size]);
+                    number_swaps++;
+                }
+            }
         }
+        print_vector(vector_seq);
     }
-
-    std::cout << "Third step\n";
-    print_vector(vector_seq);
-
-    std::vector<int>::iterator start_it = vector_seq.begin();
-    if (*(start_it+3) > *(start_it+7)) {
-        std::swap(*start_it, *(start_it+4));
-        std::swap(*(start_it+1), *(start_it+5));
-        std::swap(*(start_it+2), *(start_it+6));
-        std::swap(*(start_it+3), *(start_it+7));
-        number_swaps++;
-    }
-
-    std::cout << "4th step\n";
-    print_vector(vector_seq);
-
-
-    std::cout << " Number of swaps is " << number_swaps << std::endl;
+    std::cout << "end of last element of first is " << max_power_of_2 / 2 - 1 
+                << " and the last element of secodn is "<< max_power_of_2 - 1 << std::endl;
+    std::cout << "After last step: last1 is " << last1 << " last2 is " << last2 << std::endl;
+    std::cout << "Total swaps: " << number_swaps << std::endl;
 }
