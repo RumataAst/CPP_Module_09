1e2cf9855cfd0ca91b3ff55f83e4ca5b7a10e1bd
Andrei <krestonosa@gmail.com>
2025-03-18 20:52:14 +0100

    finishing test.cpp - creating re_order function

--- ex02/test.cpp
+++ ex02/test.cpp
@@ -12,16 +12,41 @@ std::vector<int> re_order(std::vector<int> original) {
     jacob_seq.push_back(42);
     jacob_seq.push_back(84);
 
+    std::vector<int> new_vector;
+
     size_t jacob_seq_index = 0;
-    for (size_t original_index = 0; original_index < original.size(); ++original_index) {
-        if (original_index < jacob_seq[jacob_seq_index]) {
-            
+    int     original_index = 0;
+    int     previous_value = 0;
+    int     check = 0;
+    for (size_t i = 0; i < jacob_seq.size(); ++i) {
+        
+        if (i != 0) {
+            previous_value = jacob_seq[i-1];
         }
+
+        if (jacob_seq[i] > original.size()) {
+            if (check == 0) {
+                original_index = original.size() - 1;
+                check = 1;
+            }
+            else 
+                break;
+        }
+        else {
+            original_index = jacob_seq[i] - 1;
+        }
+
+        while (original_index >= previous_value) {
+            new_vector.push_back(original[original_index]);
+            original_index--;
+        }
+        
     }
+    return new_vector;
 }
 
 int main(void) {
-    std::vector<int> original = {15, 5, 2, 33, 11, 23};
+    std::vector<int> original = {15, 5, 2, 33, 11, 23, 153, 78};
     std::vector<int> reordered_copy;
 
     reordered_copy = re_order(original);
